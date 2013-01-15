d30252f45fa826177cc69403138a0712d7343abf
Ke Chang <changke@DWMU-CZC11816S2.(none)>
2013-01-15 18:11:32 +0100

    HTML5 Boilerplate

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var showWidthInfo = function() {
+    var width = viewportSize.getWidth();
+    $('.info').html('Width: ' + width + 'px');
+}
+
+$(document).ready(function() {
+    showWidthInfo();
+});
+
+$(window).resize(function() {
+    showWidthInfo();
+});
