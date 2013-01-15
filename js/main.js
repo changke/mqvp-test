c0f8450ae19db21484ecb969910f4e793cfc191d
Ke Chang <changke@gmail.com>
2013-01-15 22:05:19 +0100

    Added info about retina display capability.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var showWidthInfo = function() {
     var width = viewportSize.getWidth();
     $('.info').html('Width: ' + width + 'px');
-}
+};
 
 $(document).ready(function() {
     showWidthInfo();
